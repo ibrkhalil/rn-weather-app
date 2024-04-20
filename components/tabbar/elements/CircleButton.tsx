a871db7e5425a9b72bd57369f51c5715f41ca759
ibrkhalil <vampirekid017@gmail.com>
2024-04-20 16:06:40 +0200

    Finalize circle button

---
+++ components/tabbar/elements/CircleButton.tsx
@@ -0,0 +1,38 @@
+ import { StyleSheet, Text, View } from 'react-native'
+ import React from 'react'
+import { Canvas, Circle, Line, LinearGradient, Shadow, vec } from '@shopify/react-native-skia';
+
+ interface CircleButtonProps {
+    radius: number;
+    pressed?: boolean;
+ }
+ 
+ const CircleButton = ({radius, pressed}:CircleButtonProps) => {
+   const diameter = 2 * radius;
+   console.log(pressed)
+    return (
+     <Canvas style={{width: diameter, height: diameter}}>
+        <Circle cx={radius} cy={radius} r={radius}>
+        <LinearGradient
+        start={vec(0,0)}
+        end={vec(diameter, diameter)}
+        colors={pressed ? ["#bbbfc7", "#f5f5f9"] : ["#f5f5f9", "#dadfe7"]} />
+        <Shadow dx={1} dy={1} blur={0.5} color={'white'} inner></Shadow>
+        </Circle>
+        <Line p1={vec(radius-radius/3, radius)}
+        p2={vec(radius+radius/3, radius)}
+        strokeCap={'round'}
+        strokeWidth={4}
+        color={'#48319d'}/>
+        <Line p1={vec(radius, radius-radius/3)}
+        p2={vec(radius, radius+radius/3)}
+        strokeCap={'round'}
+        strokeWidth={4}
+        color={'#48319d'} />
+     </Canvas>
+   )
+ }
+ 
+ export default CircleButton
+ 
+ const styles = StyleSheet.create({})
--- components/tabbar/elements/TabBarItems.tsx
+++ components/tabbar/elements/TabBarItems.tsx
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
+import { Pressable, StyleSheet, Text, View, useWindowDimensions } from 'react-native'
 import React from 'react'
 import ListIcon from '../icons/ListIcon'
 import MapIcon from '../icons/MapIcon'
 import TrapzoidBackground from './TrapzoidBackground'
+import CircleButton from './CircleButton'
 
 const TabBarItems = () => {
     const {width, height} = useWindowDimensions();
     const trapzoidWidth = width * 0.68;
     const trapzoidHeight = height  * 0.12;
-    
+    const circleRadius = (trapzoidHeight * 0.51) / 2;
+    const buttonCenterX = width / 2 - circleRadius;
+
   return (
     <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 32, justifyContent: 'space-between'}}>
       <ListIcon />
       <TrapzoidBackground width={trapzoidWidth} height={trapzoidHeight} />
+      <Pressable onPress={() => alert("Alohaaaa")} style={{...StyleSheet.absoluteFillObject, left: buttonCenterX, top: 12, width: 2 * circleRadius,
+        height: 2 * circleRadius
+      }}>
+        {({pressed}) => (
+            <CircleButton radius={circleRadius} pressed={pressed}/>
+        )}
+      </Pressable>
       <MapIcon />
     </View>
   )
