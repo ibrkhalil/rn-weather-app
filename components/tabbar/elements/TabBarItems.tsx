52fcbec1972d5c5881799c853528de5302b1dfb6
ibrkhalil <vampirekid017@gmail.com>
2024-04-20 15:20:56 +0200

    Introduce responsive FitBox container and add Trapzoid

--- components/tabbar/elements/TabBarItems.tsx
+++ components/tabbar/elements/TabBarItems.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
 import React from 'react'
 import ListIcon from '../icons/ListIcon'
 import MapIcon from '../icons/MapIcon'
+import TrapzoidBackground from './TrapzoidBackground'
 
 const TabBarItems = () => {
+    const {width, height} = useWindowDimensions();
+    const trapzoidWidth = width * 0.68;
+    const trapzoidHeight = height  * 0.12;
+    
   return (
     <View style={{flex:1, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 32, justifyContent: 'space-between'}}>
       <ListIcon />
+      <TrapzoidBackground width={trapzoidWidth} height={trapzoidHeight} />
       <MapIcon />
     </View>
   )
